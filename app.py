54ca4ecde8320c6821f1df9cb99af5543ce4e016
kashyap4kartik <kashyap4kartik@gmail.com>
2026-02-18 17:34:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -75,6 +75,9 @@ def whatsapp_reply():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+    
 
 

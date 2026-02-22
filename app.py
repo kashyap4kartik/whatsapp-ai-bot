7c976f2a681cb8a77814411ab33328770c126501
kashyap4kartik <kashyap4kartik@gmail.com>
2026-02-22 19:19:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ def whatsapp_reply():
     import os
     file_path = os.path.join(os.path.dirname(__file__), "leads.txt")
 
+   if phone:
     with open(file_path,'a') as f:
         f.write(phone + "\n")
 
@@ -78,6 +79,7 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
     
+
 
 
 
